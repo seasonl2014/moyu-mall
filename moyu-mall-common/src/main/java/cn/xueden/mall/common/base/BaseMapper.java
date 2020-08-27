5e0d1137097372f5d7aecd1ef8b8c7f56c2c42df
goodym <11184629@qq.com>
2020-08-27 10:50:55 +0800

    创建公共模块

---
+++ moyu-mall-common/src/main/java/cn/xueden/mall/common/base/BaseMapper.java
@@ -0,0 +1,42 @@
+package cn.xueden.mall.common.base;
+
+import java.util.List;
+
+/**功能描述：公共Mapper
+ * @Auther:http://www.xueden.cn
+ * @Date:2020/3/22
+ * @Description:cn.xueden.common.base
+ * @version:1.0
+ */
+public interface BaseMapper<D,E> {
+
+    /**
+     * DTO转Entity
+     * @param dto /
+     * @return /
+     */
+    E toEntity(D dto);
+
+    /**
+     * Entity转DTO
+     * @param entity /
+     * @return /
+     */
+    D toDto(E entity);
+
+    /**
+     * DTO集合转Entity集合
+     * @param dtoList /
+     * @return /
+     */
+    List<E> toEntity(List<D> dtoList);
+
+    /**
+     * Entity集合转DTO集合
+     * @param entityList /
+     * @return /
+     */
+    List <D> toDto(List<E> entityList);
+
+
+}
